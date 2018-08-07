7bdfa1ec96702e647b44748b88676f84efdbcbff
gramajox <xaviergramajo@gmail.com>
2018-08-07 12:12:15 -0300

    Gestión de olvido de contraseña y verificación de mail.

--- app/src/main/java/com/example/xgramajo/parkme_ids_2018/Login/LoginActivity.java
+++ app/src/main/java/com/example/xgramajo/parkme_ids_2018/Login/LoginActivity.java
@@ -88,7 +88,6 @@ public class LoginActivity extends AppCompatActivity {
 
                             if(task.isSuccessful()){
                                 checkEmailVerification();
-                                //sendToMain();
 
                             } else {
 
