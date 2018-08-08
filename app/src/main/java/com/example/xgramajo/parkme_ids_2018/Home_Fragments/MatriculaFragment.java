f9f048ab6d72a97d42cb726783bf7367e2e36d15
gramajox <xaviergramajo@gmail.com>
2018-08-07 23:27:45 -0300

    Commint Setup tab

--- app/src/main/java/com/example/xgramajo/parkme_ids_2018/Home_Fragments/MatriculaFragment.java
+++ app/src/main/java/com/example/xgramajo/parkme_ids_2018/Home_Fragments/MatriculaFragment.java
@@ -1,4 +1,4 @@
-package com.example.xgramajo.parkme_ids_2018.Parking_Fragments;
+package com.example.xgramajo.parkme_ids_2018.Home_Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,12 +8,13 @@ import android.view.ViewGroup;
 
 import com.example.xgramajo.parkme_ids_2018.R;
 
-public class SummaryFragment extends Fragment {
+public class MatriculaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_matricula, container, false);
+
         return view;
     }
 
