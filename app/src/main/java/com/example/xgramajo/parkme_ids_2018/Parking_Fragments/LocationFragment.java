8ef42a07e8aef753ac791dbc8b934c95ace20645
NicoMedero <nicolasmederoc@gmail.com>
2018-08-08 01:20:10 -0300

    -Agregado Maps a LocationFragment, falta utilizar la ubicación -Agregado a build.gradle la dependencia de Maps sin cambiar las de firebase y google-services-auth -Para que el fragment "map" funcione hay que poner la Key en AndroidManifest.xml, no la puse porque tiene que ser privada para nosotros. Luego hacemos una referencia a algún archivo que no subamos a git y guardamos las keys ahi.

--- app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/LocationFragment.java
+++ app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/LocationFragment.java
@@ -1,21 +1,36 @@
 package com.example.xgramajo.parkme_ids_2018.Parking_Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
 import com.example.xgramajo.parkme_ids_2018.R;
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment  {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_location, container, false);
+
         // Inflate the layout for this fragment
+
         return view;
     }
-
 }
