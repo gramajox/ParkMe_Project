344f9a1294ea35cde23665ee2748d9e7f6f9b304
juancruzpugliesse <juancruzpugliesse@gmail.com>
2018-08-10 00:20:38 -0300

    Comunicacion entre SetupFragment y SummaryFragment

--- app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/SetupFragment.java
+++ app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/SetupFragment.java
@@ -1,20 +1,36 @@
 package com.example.xgramajo.parkme_ids_2018.Parking_Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.xgramajo.parkme_ids_2018.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetupFragment extends Fragment {
 
     private static Spinner spinnerDur, spinnerPatente;
     private static TextView montoCalculado;
+    String[] arrayMontos;
+
+    String tiempoSeleccionado;
+    String numeroPatente;
+    String montoAsociado;
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,34 +41,73 @@ public class SetupFragment extends Fragment {
         spinnerDur = (Spinner) view.findViewById(R.id.spinner_duracion);
         montoCalculado = (TextView) view.findViewById(R.id.txt_monto);
 
-        String itemSelect = spinnerDur.getSelectedItem().toString();
 
-        switch(itemSelect) {
-                case "30 minutos":
-                    montoCalculado.setText("$20");
-                    break;
-                case "1 hora":
-                    montoCalculado.setText("$40");
-                    break;
-                case "1 hora 30 minutos":
-                    montoCalculado.setText("$60");
-                default:
-                    montoCalculado.setText("_______");
-        }
+
+       //Fuente: https://es.stackoverflow.com/questions/69656/evento-onclick-en-un-spinner
+        spinnerDur.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> spn,
+                                               android.view.View v,
+                                               int posicion,
+                                               long id) {
+                        tiempoSeleccionado = spn.getSelectedItem().toString();
+                        posicion = spn.getSelectedItemPosition();
+                        arrayMontos = getResources().getStringArray(R.array.array_montos);
+                        montoAsociado = arrayMontos[posicion];
+                        montoCalculado.setText(montoAsociado);
+                        }
+                    public void onNothingSelected(AdapterView<?> spn) {
+                    }
+                });
+        spinnerPatente.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> spn2,
+                                               android.view.View v2,
+                                               int position,
+                                               long id2) {
+                     numeroPatente = spn2.getSelectedItem().toString();
+
+
+                    }
+                    public void onNothingSelected(AdapterView<?> spn2) {
+                    }
+                });
+
+
+
+
+
+
 
         return view;
     }
 
-    public static String getPrice() {
+    public static String getMount(){
         return montoCalculado.getText().toString();
     }
 
-    public static String getSelectedTime() {
+    public static String getSelectedTime (){
+
         return spinnerDur.getSelectedItem().toString();
     }
 
-    public static String getPatente() {
+
+    public static String getMatricula (){
         return spinnerPatente.getSelectedItem().toString();
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/SummaryFragment.java
+++ app/src/main/java/com/example/xgramajo/parkme_ids_2018/Parking_Fragments/SummaryFragment.java
@@ -5,16 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.xgramajo.parkme_ids_2018.R;
 
 public class SummaryFragment extends Fragment {
 
+
+    private static TextView matricula;
+    private static TextView time;
+    private static TextView mount;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_summary, container, false);
+
+       matricula = (TextView) view.findViewById(R.id.txt_res_msj02);
+       matricula.setText(SetupFragment.getMatricula());
+
+        time = (TextView) view.findViewById(R.id.txt_res_msj03);
+        time.setText(SetupFragment.getSelectedTime());
+
+        mount = (TextView) view.findViewById(R.id.textView4);
+        mount.setText(SetupFragment.getMount());
+
+
         return view;
     }
+
+
 
 }
